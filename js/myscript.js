c0b2dbb301c55231a35e9fb984caa1d4c1119ceb
GaneshIT <ganesh.ceoit@gmail.com>
2023-12-07 18:50:05 +0530

    updated

--- js/myscript.js
+++ js/myscript.js
@@ -18,14 +18,65 @@ function f1(){   //function definition
 function displayEventTypes(){
     var types=["Music","Sports","Dance","Tech"]; //declare array
     var list="";
-    for(let i=0;i<types.length;i++){
-        list=list+"<li>"+types[i]+"</li>";
-    }
+    // for(let i=0;i<types.length;i++){
+    //     list=list+"<li>"+types[i]+"</li>";
+    // }
+    //array.map() function
+    types.map(function(obj,i){
+        list=list+"<li>"+obj+"</li>";
+    })
     document.getElementById('eventTypes').innerHTML=list;
     console.log(list);
 }
 
 displayEventTypes();
+
+function m1(name,myFun){
+    alert('Hai');
+    myFun();//callback function
+    alert(name);
+}
+function m2(){
+    alert('welcome');
+}
+//m1('Raj',m2)
+
+//array.filter()
+var values=[10,30,8,37,23,56];
+
+// function getValue(number){
+//     return number+10;
+// }
+// var result=getValue(200);
+// document.write(result);
+
+var output=values.filter(function(obj,i){
+    //logic
+    return obj%2==0;
+});
+//document.write(output);
+
+//array.slice(),array.splice()
+
+//syntax:
+//slice(optional start index, optional end index)
+var items=["Mi","Nokia","Sony","IPhone"]
+// var result=items.slice(1,3);
+// document.write(result);
+//syntax:
+//splice(start, delete count, items to add)
+// document.write(items.splice(1,0,"samsung"))
+// document.write("<br />")
+items.splice(1,0,"samsung");
+console.log(items);
+// document.write("<br />")
+// document.write(items.splice(2,2,"samsung"))
+// document.write("<br />")
+// document.write(items.splice(3,1,"samsung","S2"))
+
+
+Javascript
+
 
 
 
